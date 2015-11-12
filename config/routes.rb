32720d878570b9a4bc85c99f4bcb15504240048a
natashaslprice <natashaslprice@gmail.com>
2015-11-12 11:26:35 -0800

    index rendered

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,13 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  get 'welcome/index'
 
-  get 'posts/new'
-
-  get 'posts/create'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/destroy'
-
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
+  resources :users, :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
