d528f51a5e18416ebf1c96c56e242f84508949e4
natashaslprice <natashaslprice@gmail.com>
2015-11-12 14:15:41 -0800

    seed data for user 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create([
+	{
+		title: "Burritt Room + Tavern"
+		content: "Good bar with tasty cocktails. Bar snack selection limited, but waiters are attentive and will make any cocktail you want. I was there on a Friday night and the live jazz was brilliant."
+		city: "San Francisco"
+		user_id: 1
+	},
+	{
+		title: "Sushirrito"
+		content: "Sushi, the size of a burrito! What can go wrong!! Delicious lunch venue. Sumo Crunch is the best."
+		city: "San Francisco"
+		user_id: 1
+	},
+	{
+		title: "Blowfish Sushi"
+		content: "Really good sushi restaurant. Probably over-priced, but the menu is varied and the atmosphere is nice. Good date venue. The avocado/ salmon/ tuna pyramid is to die for!"
+		city: "San Francisco"
+		user_id: 1
+	},
+	{
+		title: "Osaka Sushi"
+		content: "Sushi was fine, but service pretty poor."
+		city: "San Francisco"
+		user_id: 1
+	},
+	{
+		title: "Crissy Fields"
+		content: "Small beach near the Marina in SF. Nice for a morning walk, and always full of happy people walking their dogs or playing with their kids. "
+		city: "San Francisco"
+		user_id: 1
+	}
+])
