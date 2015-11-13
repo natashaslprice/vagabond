1390054f7d7c7adc7e49d22013ff9a99ba2d48e3
natashaslprice <natashaslprice@gmail.com>
2015-11-13 11:28:38 -0800

    sf page showing sf posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,48 +3,66 @@ Post.create([
 		title: "Burritt Room + Tavern",
 		content: "Good bar with tasty cocktails. Bar snack selection limited, but waiters are attentive and will make any cocktail you want. I was there on a Friday night and the live jazz was brilliant.",
 		city: "San Francisco",
-		user_id: 1
+		user_id: 1,
+		city_id: 1
 	},
 	{
 		title: "Sushirrito",
 		content: "Sushi, the size of a burrito! What can go wrong!! Delicious lunch venue. Sumo Crunch is the best.",
 		city: "San Francisco",
-		user_id: 1
+		user_id: 1,
+		city_id: 1
 	},
 	{
 		title: "Blowfish Sushi",
 		content: "Really good sushi restaurant. Probably over-priced, but the menu is varied and the atmosphere is nice. Good date venue. The avocado/ salmon/ tuna pyramid is to die for!",
 		city: "San Francisco",
-		user_id: 1
+		user_id: 1,
+		city_id: 1
 	},
 	{
 		title: "Crissy Fields",
 		content: "Small beach near the Marina in SF. Nice for a morning walk, and always full of happy people walking their dogs or playing with their kids.",
 		city: "San Francisco",
-		user_id: 1
+		user_id: 1,
+		city_id: 1
 	},
 	{
 		title: "Osaka Sushi",
 		content: "Sushi was fine, but service pretty poor.",
 		city: "San Francisco",
-		user_id: 2
+		user_id: 2,
+		city_id: 1
 	},
 	{
 		title: "Murphy's",
 		content: "Fine. Nothing special. Pretty mellow.",
 		city: "San Francisco",
-		user_id: 2
+		user_id: 2,
+		city_id: 1
 	},
 	{
 		title: "Lush Lounge",
 		content: "Noisy and crowded. Very fun! Awesome for picking up chicks!",
 		city: "San Francisco",
-		user_id: 2
+		user_id: 2,
+		city_id: 1
 	},
 	{
 		title: "El Rio",
 		content: "The Mango event on the last Sunday of each month is fun. Lot's ",
 		city: "San Francisco",
-		user_id: 2
+		user_id: 2,
+		city_id: 1
 	},
 ])
+
+City.create([
+	{
+		name: "San Francisco",
+		pic: "http://static1.squarespace.com/static/547f29bfe4b0dc192ed7bdac/54aeb15ce4b018c14f34c7cb/54aeb160e4b018c14f34c7ed/1420734817363/san-franc.jpg?format=2500w"
+	},
+])
+
+
+
