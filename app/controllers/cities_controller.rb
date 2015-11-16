3277feb1fa482e5e36ac132b99c6d62386f19f91
ekingan <ekingan@gmail.com>
2015-11-16 11:28:03 -0800

    editing posts in page not modal, also diplaying cities and number of posts on profile correctly

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,7 +4,7 @@ class CitiesController < ApplicationController
   	@user = current_user
   	#find city by id
   	@city = City.find(params[:id]) 
-  	@posts = Post.where(:city_id => @city).paginate(page: params[:page], per_page: 15).order('created_at DESC')
+  	@posts = Post.where(:city_id => @city).paginate(page: params[:page], per_page: 10).order('created_at DESC')
   	
   end
 end
